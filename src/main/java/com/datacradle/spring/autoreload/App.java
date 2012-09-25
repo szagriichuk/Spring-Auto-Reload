dc6726ea278535e98096713ea59841cad2c815da
Sergii Zagriichuk <s.zagriichuk@datacradle.com>
2012-09-25 12:33:22 +0300

    add sources

---
+++ src/main/java/com/datacradle/spring/autoreload/App.java
@@ -0,0 +1,13 @@
+package com.datacradle.spring.autoreload;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
